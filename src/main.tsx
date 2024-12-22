deec066be69aab13721bb6d778069760ede159f6
naco0406 <young0406@kaist.ac.kr>
2024-12-22 20:49:50 +0900

    feat: Vercel Analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
+import { Analytics } from "@vercel/analytics/react"
+
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
     <ChakraProvider value={defaultSystem}>
       <App />
+      <Analytics />
     </ChakraProvider>
   // </StrictMode>
 )
